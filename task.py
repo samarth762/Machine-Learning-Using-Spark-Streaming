9dff443b49200525cb65325166ab73c6c8d3381a
Sammed D Tapakire <60000995+sammed-123@users.noreply.github.com>
2021-11-18 22:55:44 +0530

    Update task.py

--- task.py
+++ task.py
@@ -1,6 +1,22 @@
-from pyspark import SparkContext
-from pyspark.sql import SQLContext
+import findspark
+findspark.init()
+import time
+from pyspark import SparkContext,SparkConf
+from pyspark.sql import Row,SQLContext
+from pyspark.streaming import StreamingContext
 from pyspark.sql.functions import count
 import sys
+import requests
 
+#conf=SparkConf()
+#conf.setAppName("ML_with_Spark")
+sc=SparkContext(appName="test")
 
+ssc=StreamingContext(sc,2)
+ssc.checkpoint("checkPoint")
+
+datastream=ssc.socketTextStream("localhost",6100)
+datastream.pprint()
+ssc.start()
+ssc.awaitTermination(2)
+ssc.stop()
